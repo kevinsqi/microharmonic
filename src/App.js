2ea2e2e06c12c108d58404db2e664444731bc5bb
Kevin Qi <iqnivek@gmail.com>
2017-07-03 19:49:23 -0700

    WIP, produce tones

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// note - number of half steps up from A440
+function getFrequency(note) {
+  return 440 * Math.pow(2, note * (1 / 12));
+}
+
 class App extends Component {
+
+  onClick(note) {
+    var context = new window.AudioContext();
+
+    var oscillator = context.createOscillator();
+    oscillator.type = 'square';
+    oscillator.frequency.value = getFrequency(note);
+    oscillator.connect(context.destination);
+    oscillator.start(0);
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <button onClick={this.onClick.bind(this, 0)}>C</button>
+        <button onClick={this.onClick.bind(this, 1)}>D</button>
+        <button onClick={this.onClick.bind(this, 2)}>E</button>
       </div>
     );
   }
