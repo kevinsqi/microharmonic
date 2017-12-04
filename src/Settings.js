7f1de87a6e9cd5873211ad66679bb5876930b94e
Kevin Qi <iqnivek@gmail.com>
2017-12-03 21:07:52 -0800

    validate and add save step for custom tuning

--- src/Settings.js
+++ src/Settings.js
@@ -1,6 +1,10 @@
+// TODO: split equal temp settings and custom into separate components
+
 import React from 'react';
 import _ from 'lodash';
 import classNames from 'classnames';
+
+import { CENTS_IN_OCTAVE } from './noteHelpers';
 
 const MAX_NUM_STEPS = 100;
 const MAX_NUM_OCTAVES = 10;
@@ -15,12 +19,35 @@ function Label(props) {
   );
 }
 
+function parseCustomCentValuesStr(str) {
+  const values = str.trim().split(/\s+/).map((value) => parseInt(value, 10)).sort((a, b) => a - b);
+
+  if (values.some((value) => isNaN(value))) {
+    throw new Error('Has invalid value which is not a number');
+  } else if (values.some((value) => value < 0 || value > CENTS_IN_OCTAVE)) {
+    throw new Error('Has out of range cent value (must be between 0 and 1200)');
+  }
+  return values;
+}
+
+function stringifyCustomCentValues(values) {
+  return values.join('\n');
+}
+
 class Settings extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showCustomSettings: false,
+      customCentValuesStr: stringifyCustomCentValues(props.config.customCentValues),
     };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.state.customCentValuesStr !== nextProps.config.customCentValues) {
+      this.setState({
+        customCentValuesStr: stringifyCustomCentValues(nextProps.config.customCentValues),
+      });
+    }
   }
 
   reset() {
@@ -72,10 +99,20 @@ class Settings extends React.Component {
   // TODO: sort
   // TODO: add validation of NaN, <1200, etc
   onChangeCustomCentValues = (event) => {
-    const centValues = event.target.value.split(/\s/).map((value) => parseInt(value, 10));
-    this.props.setConfig({
-      customCentValues: centValues,
+    this.setState({
+      customCentValuesStr: event.target.value,
     });
+  };
+
+  onSaveCustomCentValues = (event) => {
+    try {
+      const values = parseCustomCentValuesStr(this.state.customCentValuesStr);
+      this.props.setConfig({
+        customCentValues: values,
+      });
+    } catch (error) {
+      alert('Sorry, there was an error interpreting these values.');
+    }
   };
 
   renderStepSettings() {
@@ -176,21 +213,21 @@ class Settings extends React.Component {
         <div>
           <Label>Custom scale tuning</Label>
         </div>
-        <small className="text-muted">Enter cent values between 0 and 1200, separated by line breaks or spaces</small>
+        <small className="text-muted">Enter cent values between 0 and 1200, separated by line breaks</small>
         <textarea
           className="form-control"
           rows="10"
-          value={this.props.config.customCentValues.join('\n')}
+          value={this.state.customCentValuesStr}
           onChange={this.onChangeCustomCentValues}
         />
+        <div className="mt-3">
+          <button className="btn btn-primary" onClick={this.onSaveCustomCentValues}>Save</button>
+        </div>
       </div>
     );
   }
 
   toggleCustomSettings(useCustom) {
-    this.setState({
-      showCustomSettings: useCustom,
-    });
     this.props.setConfig({
       useCustomCentValues: useCustom,
     });
@@ -199,6 +236,7 @@ class Settings extends React.Component {
   render() {
     const activeClass = 'btn-outline-primary';
     const inactiveClass = 'btn-outline-secondary';
+    const { useCustomCentValues } = this.props.config;
 
     return (
       <div className="card">
@@ -209,13 +247,13 @@ class Settings extends React.Component {
           <div className="float-right">
             <div className="btn-group">
               <button
-                className={classNames('btn btn-sm', this.state.showCustomSettings ? inactiveClass : activeClass)}
+                className={classNames('btn btn-sm', useCustomCentValues ? inactiveClass : activeClass)}
                 onClick={() => this.toggleCustomSettings(false)}
               >
                 Equal temperament
               </button>
               <button
-                className={classNames('btn btn-sm', this.state.showCustomSettings ? activeClass : inactiveClass)}
+                className={classNames('btn btn-sm', useCustomCentValues ? activeClass : inactiveClass)}
                 onClick={() => this.toggleCustomSettings(true)}
               >
                 Custom tuning
@@ -225,7 +263,7 @@ class Settings extends React.Component {
         </div>
         <div className="card-body">
           {
-            this.state.showCustomSettings ? (
+            useCustomCentValues ? (
               this.renderCustomSettings()
             ) : (
               this.renderEqualTemperamentSettings()
