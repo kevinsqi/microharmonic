bef27a5165ef08365ba4407a7b30b0cff7ed245d
Kevin Qi <iqnivek@gmail.com>
2018-03-03 16:38:41 -0800

    create express server

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from react app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api/foo', (request, response) => {
+  response.json({
+    hello: 'hi',
+  });
+});
+
+// Catchall to serve react index file
+app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
