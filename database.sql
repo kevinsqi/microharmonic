03c263adeb2bdfa784f5637b4068d937f8e8506d
Kevin Qi <iqnivek@gmail.com>
2018-03-24 22:57:37 -0700

    configure connection to db

---
+++ database.sql
@@ -0,0 +1,8 @@
+DROP DATABASE IF EXISTS microharmonic;
+CREATE DATABASE microharmonic;
+
+\connect microharmonic;
+
+CREATE TABLE compositions (
+  id INTEGER PRIMARY KEY
+);
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const db = require('./db');
 
 const app = express();
 
@@ -13,6 +15,20 @@ app.use(express.urlencoded({ extended: false }));
 
 // Serve static files from react app
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Routes
+app.get('/api/compositions/', (req, res, next) => {
+  // noreintegrate
+  db.query('SELECT * FROM compositions', [], (err, dbRes) => {
+    if (err) {
+      return next(err);
+    }
+    console.log(dbRes.rows);
+  });
+  return res.json({
+    data: [],
+  });
+});
 
 // Catchall to serve react index file
 app.get('*', (request, response) => {
