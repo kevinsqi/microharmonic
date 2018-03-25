e90a7dfad1da4cab3d4a381c56f5b21d4b778302
Kevin Qi <iqnivek@gmail.com>
2018-03-24 19:00:18 -0700

    use morgan and json middleware

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const path = require('path');
+const logger = require('morgan');
 
 const app = express();
+
+// Logging
+app.use(logger('dev'));
+
+// Parse incoming request JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Serve static files from react app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Catchall to serve react index file
 app.get('*', (request, response) => {
-  response.sendFile(path.join(__dirname, '/client/build/index.html'));
+  response.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
