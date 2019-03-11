53678c01bff526770a8c2851a3401530f03dfa45
Kevin Qi <kqi@amino.com>
2019-03-10 23:30:22 -0700

    rename references to github.com/iqnivek -> kevinsqi

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -4,19 +4,17 @@ import classNames from 'classnames';
 class Footer extends React.Component {
   render() {
     return (
-      <footer
-        className={classNames(
-          'd-flex align-items-end bg-gray',
-          this.props.className,
-        )}
-      >
+      <footer className={classNames('d-flex align-items-end bg-gray', this.props.className)}>
         <div className="container">
           <div className="d-flex text-light mt4 mb1">
             <div className="mr-auto">
-              Built by <a className="text-light" href="http://www.kevinqi.com/">Kevin Qi</a>
+              Built by{' '}
+              <a className="text-light" href="http://www.kevinqi.com/">
+                Kevin Qi
+              </a>
             </div>
             <div>
-              <a className="text-light" href="https://github.com/iqnivek/microharmonic/">
+              <a className="text-light" href="https://github.com/kevinsqi/microharmonic/">
                 View source on Github
               </a>
             </div>
